3372a4ed6e115349110deb2f37ce465386e07553
JessieDomenicActub-UCM <jessiemangz11@gmail.com>
2026-02-28 12:10:03 +0800

    Update StringService.cs

--- StringService.cs
+++ StringService.cs
@@ -1,35 +1,60 @@
-namespace StringProcessingApp.Services
+using System;
+
+namespace StringProcessingApp
 {
     public class StringService
     {
-        private string _originalText = "";
-        public string CurrentText { get; private set; } = "";
+        private string originalText = "";
+        public string CurrentText = "";
 
         public void SetText(string text)
         {
-            _originalText = text;
+            originalText = text;
             CurrentText = text;
         }
 
-        public void ToUpper() => CurrentText = CurrentText.ToUpper();
-
-        public void ToLower() => CurrentText = CurrentText.ToLower();
-
-        public int GetLength() => CurrentText.Length;
-
-        public bool ContainsWord(string word) => CurrentText.Contains(word);
-
-        public void ReplaceWord(string oldWord, string newWord) 
-            => CurrentText = CurrentText.Replace(oldWord, newWord);
-
-        public void ExtractSubstring(int start, int length)
+        public void ToUpper()
         {
-            if (start >= 0 && start + length <= CurrentText.Length)
-                CurrentText = CurrentText.Substring(start, length);
+            CurrentText = CurrentText.ToUpper();
         }
 
-        public void TrimSpaces() => CurrentText = CurrentText.Trim();
+        public void ToLower()
+        {
+            CurrentText = CurrentText.ToLower();
+        }
 
-        public void Reset() => CurrentText = _originalText;
+        public int GetLength()
+        {
+            return CurrentText.Length;
+        }
+
+        public bool ContainsWord(string word)
+        {
+            return CurrentText.Contains(word);
+        }
+
+        public void ReplaceWord(string oldWord, string newWord)
+        {
+            CurrentText = CurrentText.Replace(oldWord, newWord);
+        }
+
+        public void ExtractSubstring(int start, int len)
+        {
+
+            if (start >= 0 && (start + len) <= CurrentText.Length)
+            {
+                CurrentText = CurrentText.Substring(start, len);
+            }
+        }
+
+        public void TrimSpaces()
+        {
+            CurrentText = currentText.Trim();
+        }
+
+        public void Reset()
+        {
+            CurrentText = originalText;
+        }
     }
 }
